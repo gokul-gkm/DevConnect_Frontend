d9f8525b4db11c612df4604673ae1015494ae3f9
Gokul <128957368+gokul-gkm@users.noreply.github.com>
2026-02-09 20:00:32 +0530

    feat: added loading spinner to payment button

--- src/components/user/payments/PaymentElementForm.tsx
+++ src/components/user/payments/PaymentElementForm.tsx
@@ -4,16 +4,21 @@ import {
   useStripe,
 } from "@stripe/react-stripe-js";
 import { Button } from "@/components/ui/Button";
+import { useState } from "react";
+import { Loader2 } from "lucide-react";
 
 export function PaymentElementForm() {
   const stripe = useStripe();
   const elements = useElements();
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    
+    if (!stripe || !elements || isLoading) return;
 
-    if (!stripe || !elements) return;
-
+    setIsLoading(true);
+    
     const { error } = await stripe.confirmPayment({
       elements,
       confirmParams: {
@@ -22,6 +27,7 @@ export function PaymentElementForm() {
     });
 
     if (error) {
+      setIsLoading(false)
       console.error(error.message);
     }
   };
@@ -35,7 +41,8 @@ export function PaymentElementForm() {
           },
         }}
       />
-      <Button type="submit"  className="h-12 w-full rounded-xl dark:bg-emerald-950/50 dark:hover:bg-emerald-900/50 dark:text-emerald-400 border dark:border-emerald-500/30 shadow-lg dark:shadow-emerald-900/5 dark:hover:shadow-emerald-900/10 transition-all duration-300">
+      <Button type="submit" className="h-12 w-full rounded-xl dark:bg-emerald-950/50 dark:hover:bg-emerald-900/50 dark:text-emerald-400 border dark:border-emerald-500/30 shadow-lg dark:shadow-emerald-900/5 dark:hover:shadow-emerald-900/10 transition-all duration-300">
+        {isLoading && <Loader2 className="w-4 h-4 animate-spin mr-2" />}
         Pay now
       </Button>
     </form>
